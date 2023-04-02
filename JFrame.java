5a0d6784c466fcd8b2fa754ab045e0575fbaaac0
MeganBui13 <meganbui0702@gmail.com>
2023-04-01 21:55:01 -0500

    runner, jframe, jpanel yas

---
+++ JFrame.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.swing.*;
+public class JFrame extends javax.swing.JFrame {
+    final int WIDTH = 1600;
+    final int LENGTH = 800;
+    public JFrame(String name)
+    {
+        super(name);
+        setSize(WIDTH, LENGTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //add(new JPanel());
+        setVisible(true);
+    }
+}
