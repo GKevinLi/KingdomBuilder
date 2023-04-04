034f1e30b5452f350069ef2b1c0375fe688877d5
MeganBui13 <meganbui0702@gmail.com>
2023-04-04 08:04:39 -0500

    yas queen slay

--- StartScreenPanel.java
+++ StartScreenPanel.java
@@ -1,21 +1,21 @@
-import java.awt.*;
-import java.awt.image.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.io.*;
-import java.awt.event.MouseListener;
+import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.util.*;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 
 public class StartScreenPanel extends JPanel implements MouseListener{
     private BufferedImage background;
+    private int event;
     public StartScreenPanel()
     {
+        event = 0;
         try
         {
             background = ImageIO.read(StartScreenPanel.class.getResource("/deez imgs/image (1).png"));
         }
-        catch (java.lang.Exception e)
+        catch (Exception e)
         {
             System.out.println("exception error");
             return;
@@ -25,6 +25,11 @@ public class StartScreenPanel extends JPanel implements MouseListener{
     public void paint(Graphics g)
     {
         g.drawImage(background, 0,0,getWidth(),getHeight(),null);
+        if(event == 0)
+        {
+            showStartingScreen(g);
+
+        }
     }
 
     public void showRules()
@@ -32,9 +37,10 @@ public class StartScreenPanel extends JPanel implements MouseListener{
         //find good pdf
     }
 
-    public void showStartingScreen()
+    public void showStartingScreen(Graphics g)
     {
-
+        g.setColor(Color.gray);
+        g.fillRect(getWidth()/2, getHeight()/4, 300, 100);
     }
 
     public void mousePressed(MouseEvent e) {}
