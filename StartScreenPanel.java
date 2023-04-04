20fb7816aae3ff8973477ea8f3addcb8874d01dc
MeganBui13 <meganbui0702@gmail.com>
2023-04-04 07:26:36 -0500

    yas queen

--- StartScreenPanel.java
+++ StartScreenPanel.java
@@ -4,13 +4,11 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.io.*;
 import java.awt.event.MouseListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.util.*;
 
 public class StartScreenPanel extends JPanel implements MouseListener{
-    private BufferedImages background;
+    private BufferedImage background;
     public StartScreenPanel()
     {
         try
