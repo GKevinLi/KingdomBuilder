85630603fa8e0ec64487d01d65130d1f55080a8d
Megan B <77942699+MeganBui13@users.noreply.github.com>
2023-04-21 01:35:19 -0500

    Update ActionToken.java

--- ActionToken.java
+++ ActionToken.java
@@ -1,42 +1,58 @@
-public class ActionToken {
-    private String action;
-    private boolean usedAlready;
-    public ActionToken(String s) {
-        action = s;
+import java.util.ArrayList;
+
+public class Player {
+    private String playerName;
+    private int score;
+    private ArrayList<Settlement> houses;
+    private ArrayList<ActionToken> specialActions;
+    private boolean isFirstPlayer;
+    private TerrainCard terrainCard;
+
+    public Player(String s)
+    {
+        playerName = s;
+        score = 0;
+        houses = new ArrayList<Settlement>();
+        specialActions = new ArrayList<ActionToken>();
+        isFirstPlayer = false;
+        //terrainCard = new TerrainCard();
     }
-    public String getAction() {
-        return action;
+    public void addScore(int a) {
+        score += a;
     }
-    public boolean getUsed() {
-        return usedAlready;
+    public int getScore() {
+        return score;
     }
-    public void switchUsed() {
-        usedAlready = !(usedAlready);
+    public ArrayList<Settlement> getHouses() {
+        return houses;
     }
-    public void useAction(Player p) {
-        if(action.equals("Paddock")) {
-
-        }
-        if(action.equals("Farm")) {
-
-        }
-        if(action.equals("Oasis")) {
-
-        }
-        if(action.equals("Oracle")) {
-
-        }
-        if(action.equals("Tower")) {
-
-        }
-        if(action.equals("Tavern")) {
-
-        }
-        if(action.equals("Barn")) {
-
-        }
-        if(action.equals("Harbor")) {
-
-        }
+    public boolean getFirstPlayer() {
+        return isFirstPlayer;
     }
+    public TerrainCard getTerrainCard() {
+        return terrainCard;
+    }
+    public ArrayList<ActionToken> getSpecialActions() { return specialActions; }
+    public void setTerrainCard(TerrainCard t) {
+        terrainCard = t;
+    }
+    public void addSpecialAction(ActionToken t) {
+        specialActions.add(t);
+    }
+    public ArrayList<Tile> getAllAdjacentTiles() {
+        ArrayList<Tile> t = new ArrayList<>();
+        for(Settlement s : houses) {
+            ArrayList<Tile> adj = s.getPlacedOn().getAdjacentTiles();
+            for(Tile i : adj) {
+                if(!(i.HouseCheck()) && (i.getHousePlaceable())) {
+                    t.add(i);
+                }
+            }
+        }
+        return t;
+    }
+    public String getName() {
+        return playerName;
+    }
+
 }
