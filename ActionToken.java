c9a2e69545eb9d80d6f3fa27575dd469c2781edd
Megan B <77942699+MeganBui13@users.noreply.github.com>
2023-04-21 01:36:55 -0500

    Update ActionToken.java

--- ActionToken.java
+++ ActionToken.java
@@ -1,58 +1,42 @@
-import java.util.ArrayList;
+public class ActionToken {
+    private String action;
+    private boolean usedAlready;
+    public ActionToken(String s) {
+        action = s;
+    }
+    public String getAction() {
+        return action;
+    }
+    public boolean getUsed() {
+        return usedAlready;
+    }
+    public void switchUsed() {
+        usedAlready = !(usedAlready);
+    }
+    public void useAction(Player p) {
+        if(action.equals("Paddock")) {
 
-public class Player {
-    private String playerName;
-    private int score;
-    private ArrayList<Settlement> houses;
-    private ArrayList<ActionToken> specialActions;
-    private boolean isFirstPlayer;
-    private TerrainCard terrainCard;
-
-    public Player(String s)
-    {
-        playerName = s;
-        score = 0;
-        houses = new ArrayList<Settlement>();
-        specialActions = new ArrayList<ActionToken>();
-        isFirstPlayer = false;
-        //terrainCard = new TerrainCard();
-    }
-    public void addScore(int a) {
-        score += a;
-    }
-    public int getScore() {
-        return score;
-    }
-    public ArrayList<Settlement> getHouses() {
-        return houses;
-    }
-    public boolean getFirstPlayer() {
-        return isFirstPlayer;
-    }
-    public TerrainCard getTerrainCard() {
-        return terrainCard;
-    }
-    public ArrayList<ActionToken> getSpecialActions() { return specialActions; }
-    public void setTerrainCard(TerrainCard t) {
-        terrainCard = t;
-    }
-    public void addSpecialAction(ActionToken t) {
-        specialActions.add(t);
-    }
-    public ArrayList<Tile> getAllAdjacentTiles() {
-        ArrayList<Tile> t = new ArrayList<>();
-        for(Settlement s : houses) {
-            ArrayList<Tile> adj = s.getPlacedOn().getAdjacentTiles();
-            for(Tile i : adj) {
-                if(!(i.HouseCheck()) && (i.getHousePlaceable())) {
-                    t.add(i);
-                }
-            }
         }
-        return t;
-    }
-    public String getName() {
-        return playerName;
-    }
+        if(action.equals("Farm")) {
 
+        }
+        if(action.equals("Oasis")) {
+
+        }
+        if(action.equals("Oracle")) {
+
+        }
+        if(action.equals("Tower")) {
+
+        }
+        if(action.equals("Tavern")) {
+
+        }
+        if(action.equals("Barn")) {
+
+        }
+        if(action.equals("Harbor")) {
+
+        }
+    }
 }
