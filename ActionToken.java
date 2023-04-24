7af40cebb05bcfe5a7ed8295a11b2e1908b851e8
IsGarbage <89527356+GKevinLi@users.noreply.github.com>
2023-04-23 23:24:18 -0500

    Update ActionToken.java

--- ActionToken.java
+++ ActionToken.java
@@ -1,21 +1,43 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class ActionToken {
     private String action;
     private boolean usedAlready;
-    public ActionToken(String s) {
+    private int id;
+    private BufferedImage img;
+    public ActionToken(String s, int i) {
         action = s;
+        id = i;
+        s = s.toLowerCase();
+        usedAlready = true;
+        try {
+			img = ImageIO.read(GameScreenPanel.class.getResource("/deez imgs/KB-" + s + ".png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     public String getAction() {
         return action;
     }
+    public int getID() {
+    	return id;
+    }
     public boolean getUsed() {
         return usedAlready;
+    }
+    public BufferedImage getImage() {
+    	return img;
     }
     public void switchUsed() {
         usedAlready = !(usedAlready);
     }
     public void useAction(Player p) {
         if(action.equals("Paddock")) {
-
+        		
         }
         if(action.equals("Farm")) {
 
